d48f03f9ee990dace16aa11bbbaf946d2d1d8c67
iballi2017 <iballi2017@gmail.com>
2021-05-16 05:04:51 +0100

    new commit

--- javascript/main.jquery.js
+++ javascript/main.jquery.js
@@ -13,28 +13,86 @@ $(document).ready(function() {
 
 
 
-    var whatWeDoSlider = $('#what-we-do-slider');
-    whatWeDoSlider.owlCarousel({
+    var servicesSlider = $('#services-slider');
+    servicesSlider.owlCarousel({
         margin: 20,
         dots: false,
+        loop: true,
+        auto: true,
+        speed: 1000,
+        slideTransition: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4500,
+        lazyLoad: true,
+        autoplayTimeout: 6000,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
                 dots: true
             },
             600: {
+                items: 2
+            },
+            1000: {
                 items: 3
             }
         }
-    });
+    }, $(".services-slider-control").removeClass("d-none"));
     // Go to the next item
     $('.services-slider-control .next-control').click(function() {
-            whatWeDoSlider.trigger('next.owl.carousel');
+            servicesSlider.trigger('next.owl.carousel');
         })
         // Go to the previous item
     $('.services-slider-control .prev-control').click(function() {
         // With optional speed parameter
         // Parameters has to be in square bracket '[]'
-        whatWeDoSlider.trigger('prev.owl.carousel', [300]);
-    })
+        servicesSlider.trigger('prev.owl.carousel', [300]);
+    });
+
+    var whyChooseUsSlider = $("#why-choose-us-slider");
+    whyChooseUsSlider.owlCarousel({
+            animateOut: 'animate__fadeIn',
+            animateIn: 'animate__fadeOut',
+            loop: true,
+            dots: false,
+            speed: 1000,
+            slideTransition: 'linear',
+            // autoplay: true,
+            autoplaySpeed: 4500,
+            lazyLoad: true,
+            autoplayTimeout: 6000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                }
+            }
+        },
+        $(".feature-images-slider-control").removeClass("d-none")
+    );
+    // Go to the next item
+    $('.feature-images-slider-control .next-control').click(function() {
+            whyChooseUsSlider.trigger('next.owl.carousel');
+        })
+        // Go to the previous item
+    $('.feature-images-slider-control .prev-control').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        whyChooseUsSlider.trigger('prev.owl.carousel', [300]);
+    });
+
+
+    /* Jquery.lazy*/
+    // $(function() {
+    //     $('.lazy').lazy({
+    //         visibleOnly: true,
+    //         effect: "fadeIn",
+    //         effectTime: 2000,
+    //         threshold: 0,
+    //         onError: function(element) {
+    //             console.log('error loading ' + element.data('src'));
+    //         }
+    //     });
+    // });
 });
