88983c4e862a8a4e0796be4de5ce681f62dfd32d
iballi2017 <iballi2017@gmail.com>
2021-05-17 15:42:47 +0100

    about us page

---
+++ pages/about-us.php
@@ -0,0 +1,174 @@
+<!-- header -->
+<?php
+include '../components/header.php'
+?>
+
+<div id="about-us-page">
+    <div class="bg-white">
+        <div class="container">
+            <!-- breadcrumb -->
+            <ul class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li>What we do</li>
+            </ul>
+            <div class="title-wrapper">
+                <hr class="my-0">
+                <h1 class="title mb-0">WHAT WE DO</h1>
+                <hr class="my-0">
+            </div>
+
+            <div id="highlight">
+                <div class="inner">
+                    <div class="img-wrapper">
+                        <img src="../assets/images/featured-image-1.png" alt="" class="img-fluid">
+                    </div>
+                    <p class="text-highlight">Mainland Solar is a fast growing provider of solar energy in Lagos,
+                        Nigeria. The driving force of Mainland Solar is to first solve
+                        the problem of poor supply of energy by making it affordable and available to you. Our target is
+                        to ensure we become the
+                        number one Solar Energy company by supplying not just to households but to corporate entities
+                        all over the
+                        country.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section id="value-offered-section">
+        <div class="container auto-wrapper">
+            <h4 class="title text-center">Our value offerings</h4>
+            <div class="row no-gutters">
+                <div class="col-6 col-md-4">
+                    <div class=" feature-card">
+                        <div class="inner">
+                            <div class="front-side">
+                                <img src="../assets/images/feature-img-thumbnail-1.png" alt="featured image" class="img-fluid">
+
+                                <div class="caption">
+                                    <p>Energy Audit</p>
+                                </div>
+                            </div>
+                            <div class="back-side">
+                                <p>A detailed energy audit will allow us
+                                    recommend ways to get more out of your solar
+                                    system.
+                                </p>
+                                <p>
+                                    <a href="#">Click here to request
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-4">
+                    <div class=" feature-card">
+                        <div class="inner">
+                            <div class="front-side">
+                                <img src="../assets/images/feature-img-thumbnail-2.png" alt="featured image" class="img-fluid">
+
+                                <div class="caption">
+                                    <p>Product Sales</p>
+                                </div>
+                            </div>
+                            <div class="back-side">
+                                <p>We provide you with dynamic products that
+                                    would suit your needs with competitive prices.
+                                </p>
+                                <p>
+                                    <a href="#">Click here to request
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-4">
+                    <div class=" feature-card">
+                        <div class="inner">
+                            <div class="front-side">
+                                <img src="../assets/images/feature-img-thumbnail-3.png" alt="featured image" class="img-fluid">
+
+                                <div class="caption">
+                                    <p>Product Delivery</p>
+                                </div>
+                            </div>
+                            <div class="back-side">
+                                <p>We give the convenience of door-to-door
+                                    delivery, even to nearby locations.
+                                </p>
+                                <p>
+                                    <a href="#">Click here to request
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-6 col-md-4">
+                    <div class=" feature-card">
+                        <div class="inner">
+                            <div class="front-side">
+                                <img src="../assets/images/feature-img-thumbnail-4.png" alt="featured image" class="img-fluid">
+
+                                <div class="caption">
+                                    <p>System Installation</p>
+                                </div>
+                            </div>
+                            <div class="back-side">
+                                <p>With our well trained experts, we offer expert
+                                    inverter, battery and solar panel installations
+                                    with a warranty of a year.
+                                </p>
+                                <p>
+                                    <a href="#">Click here to request
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-6 col-md-4">
+                    <div class=" feature-card">
+                        <div class="inner">
+                            <div class="front-side">
+                                <img src="../assets/images/feature-img-thumbnail-5.png" alt="featured image" class="img-fluid">
+
+                                <div class="caption">
+                                    <p>Consultation and Support</p>
+                                </div>
+                            </div>
+                            <div class="back-side">
+                                <p>We offer after sales and consultation services on
+                                    your solar energy system because you need
+                                    professional hands to maintain your product.
+                                    Battery replacements, repair and upgrade
+                                    services are also available.
+                                <p>
+                                    <a href="#">Click here to request
+                                    </a>
+                                </p>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<!-- footer-full -->
+<?php
+include '../components/footer.php'
+?>
