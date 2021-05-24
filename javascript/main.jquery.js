ea35e9d5d6c6bb82779197f5eb183581dceae2f1
iballi2017 <iballi2017@gmail.com>
2021-05-24 04:23:34 +0100

    new commit

--- javascript/main.jquery.js
+++ javascript/main.jquery.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     /* page loader */
 var loading = $("#loading");
+  $(window).on('load',function() {
 if(loading){
-    loading.addClass("d-none");
+    loading.hide();
 }
+  });
 
     $('.slider').bxSlider({
         mode: 'fade',
