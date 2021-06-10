814d2b724f0d2be2d4210f3b44e171a64add0b8d
iballi2017 <iballi2017@gmail.com>
2021-06-10 20:58:31 +0100

    adding delete buttons to lad-system-questionnaire step 2 and adding 404 page

--- pages/page-not-found.php
+++ pages/page-not-found.php
@@ -1,25 +1,61 @@
-<?php
-include '../components/header.php'
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div id="page-not-found-page">
-    <div class="bg-white h-100">
-        <div class="">
-            <div class="content text-center">
-                <div>
-                    <p class="status bold">404</p>
-                    <h1 class="title">Page Not Found</h1>
-                    <p>
-                        <a href="../" class="dark-link">Return to Home</a>
-                    </p>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mainland Solar | Your No 1 choice for Solar Installations & Support</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" class="img-fluid" href="../assets/images/navbrand-logo-min.png" class="img-fluid" sizes="16x16">
+    <!-- meta tags -->
+    <meta name="description" content="Your No 1 choice for Solar Installations & Support">
+    <!-- main font family -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+    <!-- aos animation -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- bxSlider -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+    <link rel="stylesheet" href="../assets/plugins/bxSlider/dist/js/jquery.bxslider.min.css">
+    <!-- Owl carousel2 2.4.3 -->
+    <link rel="stylesheet" href="../assets/plugins/owlCarousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="../assets/plugins/owlCarousel/owl.theme.default.min.css" />
+
+    <!-- fontawesome 5.15.3 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <!-- bootstrap 4.6.0 css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../assets/plugins/bootstrap-4.6/dist/css/bootstrap.min.css">
+    <!-- custom css -->
+    <link rel="stylesheet" href="../stylesheet/style.min.css" />
+</head>
+
+<body>
+    <div id="loading">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+
+    <div id="page-not-found-page">
+        <div class="bg-white h-100">
+            <div class="">
+                <div class="content text-center">
+                    <div>
+                        <p class="status bold">404</p>
+                        <h1 class="title">Page Not Found</h1>
+                        <p>
+                            <a href="../" class="dark-link">Return to Home</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<!-- footer-full -->
-<?php
-include '../components/footer.php'
-?>
+    <!-- footer-full -->
+    <?php
+    include '../components/footer.php'
+    ?>
